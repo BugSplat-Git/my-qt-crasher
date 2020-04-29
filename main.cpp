0d55e0489d2813d31fb06fee5f4502362c122ec4
Bobby Galli <bobbyg603@gmail.com>
2020-04-29 13:11:36 -0400

    Added StringType and CharType to move toward Windows support

--- main.cpp
+++ main.cpp
@@ -10,18 +10,26 @@
 #include "client/crashpad_client.h"
 #include "client/settings.h"
 
+#if defined(OS_POSIX)
+typedef char CharType;
+typedef std::string StringType;
+#elif defined(OS_WIN)
+typedef wchar_t CharType;
+typedef std::wstring StringType;
+#endif
+
 using namespace base;
 using namespace crashpad;
 using namespace std;
 
-bool initializeCrashpad(char *dbName, char *appName, char *appVersion);
-string getExecutableDir(void);
+bool initializeCrashpad(CharType *dbName, CharType *appName, CharType *appVersion);
+StringType getExecutableDir(void);
 
 int main(int argc, char *argv[])
 {
-    char *dbName = (char *)"Fred";
-    char *appName = (char *)"myQtCrasher";
-    char *appVersion = (char *)"1.0";
+    CharType *dbName = (CharType *)"Fred";
+    CharType *appName = (CharType *)"myQtCrasher";
+    CharType *appVersion = (CharType *)"1.0";
 
     initializeCrashpad(dbName, appName, appVersion);
 
@@ -31,10 +39,10 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
-bool initializeCrashpad(char *dbName, char *appName, char *appVersion )
+bool initializeCrashpad(CharType *dbName, CharType *appName, CharType *appVersion )
 {
     // Get directory where the exe lives so we can pass a full path to handler, reportsDir and metricsDir
-    string exeDir = getExecutableDir();
+    StringType exeDir = getExecutableDir();
 
     // Ensure that handler is shipped with your application
     FilePath handler(exeDir + "/../../../crashpad/crashpad_handler");
@@ -46,13 +54,13 @@ bool initializeCrashpad(char *dbName, char *appName, char *appVersion )
     FilePath metricsDir(exeDir + "/../../../crashpad");
 
     // Configure url with your BugSplat database
-    string url;
+    StringType url;
     url = "https://";
     url += dbName;
     url += ".bugsplat.com/post/bp/crash/crashpad.php";
 
     // Metadata that will be posted to BugSplat
-    map<string, string> annotations;
+    map<StringType, StringType> annotations;
     annotations["format"] = "minidump";                 // Required: Crashpad setting to save crash as a minidump
     annotations["product"].assign(appName);             // Required: BugSplat appName
     annotations["version"].assign(appVersion);      	// Required: BugSplat appVersion
@@ -61,7 +69,7 @@ bool initializeCrashpad(char *dbName, char *appName, char *appVersion )
     annotations["list_annotations"] = "Sample comment";	// Optional: BugSplat crash description
 
     // Disable crashpad rate limiting so that all crashes have dmp files
-    vector<string> arguments;
+    vector<StringType> arguments;
     arguments.push_back("--no-rate-limit");
 
     // Initialize crashpad database
@@ -79,7 +87,7 @@ bool initializeCrashpad(char *dbName, char *appName, char *appVersion )
     return status;
 }
 
-string getExecutableDir() {
+StringType getExecutableDir() {
     unsigned int bufferSize = 512;
     vector<char> buffer(bufferSize + 1);
 
