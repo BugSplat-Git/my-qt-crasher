d85e02bea82c9244f5de1bc367aab462846c11c4
Bobby Galli <bobbyg603@gmail.com>
2020-06-16 14:27:30 -0400

    Added symbol uploads to Windows. Added paths helper class.

---
+++ paths.h
@@ -0,0 +1,25 @@
+#ifndef PATHS_H
+#define PATHS_H
+
+#include <QApplication>
+using namespace std;
+
+class Paths
+{
+    QString m_exeDir;
+
+    public:
+        Paths(QString exeDir);
+        QString getHandlerPath();
+        QString getReportsPath();
+        QString getMetricsPath();
+        #if defined(Q_OS_MACOS)
+            static string getPlatformString(QString string);
+        #elif defined(Q_OS_WINDOWS)
+            static wstring getPlatformString(QString string);
+        #else
+            #error getPlatformStrinn not implemented on this platform
+        #endif
+};
+
+#endif // PATHS_H
