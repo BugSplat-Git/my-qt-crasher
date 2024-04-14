6b09160e503f17668fb95261aaca25234b68dc0c
Bobby Galli <bobbyg603@gmail.com>
2024-04-14 11:09:57 -0400

    chore: update symbols.bat

--- Crashpad/Tools/Windows/symbols.bat
+++ Crashpad/Tools/Windows/symbols.bat
@@ -1,1 +1,1 @@
-%1\Crashpad\Tools\Windows\symbol-upload-windows.exe -b %3 -a "%4" -v "%5" -d "%2" -f "%4.exe" -u "%6" -p "%7" --dumpSyms
+%1\Crashpad\Tools\Windows\symbol-upload-windows.exe -b %3 -a "%4" -v "%5" -d "%2" -f "%4.exe" -u "%6" -p "%7" -m
